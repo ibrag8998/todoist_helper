d6f1c1d67a1738f544fd38c914108cf46d1e10d4
ibrag8998 <ibragdzh@gmail.com>
2019-12-11 10:33:35 +0300

    faster gui loading

--- quick_add.py
+++ quick_add.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 from todoist_helper.ui import UI
-from todoist_helper.todoist_push import push
-from todoist_helper import config as cf
 
 
 def save(task):
     push(task)
 
 
+imported = False
 while True:
     ui = UI()
     ui.quick_add()
     ui.mainloop()
+
+    if not imported: # it was made because gui window appearance was very slow, now its better
+        from todoist_helper.todoist_push import push
+        imported = True
 
     if ui.task.get():
         save(ui.task.get())
